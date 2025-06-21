cf508c0a74cf0c5a7da6ce68355bfaa4db62dbb7
kendra-eck <90447407+KeeckAOE@users.noreply.github.com>
2025-06-21 19:27:37 +0200

    Setting up repository for PyPi

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="st7789vw",
+    name="JoyIT_st7789vw",
     version="1.0.0",
     description="Python-Treiber 1.3 ST7789VW TFT-Display (Joy-IT kompatibel)",
     author="Joy-IT",
     author_email="service@joy-it.net",
     license="MIT",
-    packages=["st7789vw"],
+    packages=["JoyIT_st7789vw"],
     install_requires=[
         "gpiozero",
         "spidev",
