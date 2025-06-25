a108098050f8f8bff8acf99b615c7b79220cd826
KeeckAOE <90447407+KeeckAOE@users.noreply.github.com>
2025-06-25 09:22:48 +0200

    Setting up repository for PyPi

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 setup(
     name="JoyIT_st7789vw",
     version="1.0.0",
-    description="Python-Treiber 1.3 ST7789VW TFT-Display (Joy-IT kompatibel)",
+    description="Library to use the SBC-LCD01 with the Raspberry Pi",
     author="Joy-IT",
     author_email="service@joy-it.net",
     license="MIT",
-    packages=["JoyIT_st7789vw"],
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/joy-it/JoyIT_st7789vw',  
     install_requires=[
         "gpiozero",
         "spidev",
@@ -16,7 +19,9 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.7',
+    zip_safe=False,
 )
